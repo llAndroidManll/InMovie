8890800acec1505863e43716d3f3f6b09721833c
llAndroidManll <sahakyansahak404@gmail.com>
2024-10-15 19:08:21 +0400

    Testing Async Image

--- app/src/main/java/com/sahakyan/inmovie/presentation/ui/home/HomeScreen.kt
+++ app/src/main/java/com/sahakyan/inmovie/presentation/ui/home/HomeScreen.kt
@@ -1,10 +1,19 @@
 package com.sahakyan.inmovie.presentation.ui.home
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.sahakyan.inmovie.domain.data.PopularData
 import com.sahakyan.inmovie.domain.data.PopularMovie
 import com.sahakyan.inmovie.presentation.state.CustomState
@@ -28,7 +37,7 @@ fun HomeScreen(
             customState.data != null -> {
                 Log.d(Constants.LOG_TAG, "HomeScreen: Data Loaded -> ${customState.data}")
                 // Display first movie's title as an example
-                Text(text = customState.data.results[0].title)
+                HomeScreenMain(customState.data)
             }
             else -> {
                 Log.d(Constants.LOG_TAG, "HomeScreen: No Data Available")
@@ -38,6 +47,26 @@ fun HomeScreen(
     } ?: run {
         Log.e(Constants.LOG_TAG, "HomeScreen: State is null")
         Text(text = "State is null")
+    }
+}
+
+@Composable
+fun HomeScreenMain(
+    data: PopularData<PopularMovie>
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+
+        AsyncImage(
+            model = data.results[3].posterPath,
+            contentDescription = data.results[0].title,
+            modifier = Modifier.size(30.dp),
+        )
+
+        Text(data.results[3].posterPath)
     }
 }
 
